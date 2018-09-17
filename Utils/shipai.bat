bd091e2d2d11beb087376777136a3d4bd57b4b26
poyauwa <tsukasawa_agu@yahoo.co.jp>
2018-09-18 02:18:11 +0900

    起動後即落ちするエラーを修正

--- Utils/shipai.bat
+++ Utils/shipai.bat
@@ -1,10 +1,13 @@
 if "%2" == "" call Errors/occured invalid_arguments
 set /a _n=%2-1
+
 for /l %%_ in (0,1,%_n%) do (
 	set /a _d=%%_^&3
 	set /a %1[%%_]=11+%%_/36+%%_/4
-	if !%1[%%_]:~1,1!!_d! equ 50 if %%_ lss 108 set /a %1[%%_]=%1[%%_]/10*10
+	set _d=!%1[%%_]:~1,1!!_d!
+	if !_d! equ 50 if %%_ lss 108 set /a %1[%%_]=%1[%%_]/10*10
 )
+
 for /l %%_ in (%_n%,-1,1) do (
 	set /a _m=!random!%%^(%%_+1^)
 	set /a _tmp=%1[!_m!]
