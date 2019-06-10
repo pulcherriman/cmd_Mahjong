2685a7fe45df1b66798089e72d43573f3f80b366
fvh_P <ko_kurihara@yahoo.co.jp>
2019-06-10 15:05:26 +0900

    bugfix

--- HandTools/lib/isStraight.bat
+++ HandTools/lib/isStraight.bat
@@ -1,19 +1,21 @@
 :isStraight::string ord, int ord_cnt -> int
   setlocal
-  set ord=%~2
+  set ord=%~1
   if %2 equ 3 (
-    for /l %%o in (%ord%) do (
-      set tmp=!tmp!%%o
+    for %%o in (%ord%) do (
+      set _tmp=!_tmp!%%o
     )
-    set /a tmp=tmp%%101010%%10101
-    if !tmp! equ 306 exit /b 256 ::一気通貫
+    echo !_tmp!
+    set /a _tmp="_tmp%%101010%%10101"
+    if !_tmp! equ 306 exit /b 256 ::一気通貫
   ) else if %2 equ 4 (
     for /l %%i in (0,3,9) do (
+      set _tmp=
       for /l %%j in (0,3,9) do (
-        if %%i neq %%j set tmp=!tmp!!ord:~%%j,2!
+        if %%i neq %%j set _tmp=!_tmp!!ord:~%%j,2!
       )
-      set /a tmp=tmp%%101010%%10101
-      if !tmp! equ 306 exit /b 256 ::一気通貫
+      set /a _tmp="_tmp%%101010%%10101"
+      if !_tmp! equ 306 exit /b 256 ::一気通貫
     )
   )
 exit /b 0
--- HandTools/lib/isTerminalHonor.bat
+++ HandTools/lib/isTerminalHonor.bat
@@ -2,10 +2,10 @@
   setlocal
   set /a tmp="(1-%1%%10)*(%1%%10%%9)*(%1/10%%10%%4)"
   if %tmp% neq 0 ( exit /b 0 )
-  for %%t in (%~1) do (
-    set /a tailnum="%%t%%10"
-    if !tailnum! neq 1 if !tailnum! neq 9 if %%t lss 40 ( exit /b 0 )
-    if %%t geq 40 ( exit /b 1024 ::¬˜V“ª)
+  set /a tmp=0
+  for %%t in (%~2) do (
+    set /a tmp="(1-%%t%%10)*(%%t%%10%%9)*(%%t/10%%10%%4)"
+    if !tmp! neq 0 ( exit /b 0 )
   )
-  if %head% geq 40 ( exit /b 1024 ::¬˜V“ª)
+  if %1 geq 40 ( exit /b 1024 ::¬˜V“ª)
 exit /b 67108864 ::´˜V“ª
